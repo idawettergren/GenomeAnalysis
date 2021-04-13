0fae18855f44e224b8f951a3be9ecceac5014fca
Ida Wettergren <ida@rackham2.uppmax.uu.se>
2021-04-13 05:30:23 +0200

    Added dir with scrips/commands

---
+++ code/genome_assembly.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH -A g2021012
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 22:00:00
+#SBATCH -J canu_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user ida.wettergren.8542@student.uu.se
+
+# Load canu module
+module load bioinfo-tools
+module load canu/2.0
+
+# Run canu
+canu -num_threads 4 -p 01_canu_assembly -d /home/ida/genome_analysis/project/analysis/genome_assembly/canu \
+genomeSize=24.2m -pacbio-raw \
+/home/ida/genome_analysis/project/data/raw_data/pacbio/SRR6037732_scaffold_10.fq.gz
+
