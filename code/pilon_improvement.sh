5d0ac379b788febfb667043956d94227a95294b3
idawettergren <78533386+idawettergren@users.noreply.github.com>
2021-04-16 00:43:07 +0200

    updated path

--- code/pilon_improvement.sh
+++ code/pilon_improvement.sh
@@ -14,20 +14,18 @@ module load samtools
 module load Pilon/1.24
 
 # Move to directory with .bam file
-cd /home/ida/genome_analysis/project/data/alignments/
+cd /home/ida/genome_analysis/project/data/alignment/illumina_pacbio/
 
 # Sort .bam file and create index
 samtools sort -@ 2 illumina_pacbio_alignment.bam -o illumina_pacbio_alignment_sorted.bam
 samtools index illumina_pacbio_alignment_sorted.bam
 
 # Paths to files
-assembly="/home/ida/genome_analysis/project/data/assemblies/pacbio_canu_assembly/02_canu_assembly.contigs.fasta"
-sorted_bam="/home/ida/genome_analysis/project/data/alignments/illumina_pacbio_alignment_sorted.bam"
+assembly="/home/ida/genome_analysis/project/data/assembly/pacbio_canu/02_canu_assembly.contigs.fasta"
+sorted_bam="/home/ida/genome_analysis/project/data/alignment/illumina_pacbio/illumina_pacbio_alignment_sorted.bam"
 
 # Move to output directory
-cd /home/ida/genome_analysis/project/data/assemblies/pilon_improvement/
+cd /home/ida/genome_analysis/project/data/assembly/pilon_improvement/
 
 # Run Pilon on the sorted assembly to improve it (incomplete)
 pilon --threads 2 --changes --output improved_pacbio_assembly --genome $assembly --frags $sorted_bam
-
-
