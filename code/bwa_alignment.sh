90eff0ceb5dd09019a6630a70700c0fbb509895a
idawettergren <78533386+idawettergren@users.noreply.github.com>
2021-04-15 08:57:01 +0200

    added bwa commands

--- code/bwa_alignment.sh
+++ code/bwa_alignment.sh
@@ -10,7 +10,7 @@
 
 # Load modules
 module load bioinfo-tools
-module load bwa/0.17.7
+module load bwa/0.7.17
 
 # Paths to the data
 illumina_1="/home/ida/genome_analysis/project/data/raw_data/illumina/SRR6058604_scaffold_10.1P.fastq.gz"
@@ -18,6 +18,18 @@ illumina_2="/home/ida/genome_analysis/project/data/raw_data/illumina/SRR6058604_
 assembly="/home/ida/genome_analysis/project/data/assemblies/pacbio_canu_assembly/02_canu_assembly.contigs.fasta"
 
 # Move to output directory
-cd /home/ida/genome_analysis/project/GenomeAnalysis/analysis/alignment/
+cd /home/ida/genome_analysis/project/GenomeAnalysis/analysis/alignment/bwa/
 
-# Run BWA
+# Align BWA with Illumina short reads
+# Create index
+bwa index $assembly
+
+# Align each Illumina read
+bwa aln -t 2 $assembly $illumina_1 > aln_illumina_1.sai
+bwa aln -t 2 $assembly $illumina_2 > aln_illumina_2.sai
+
+# Generate alignment from the reads
+bwa sampe $assembly aln_illumina_1.sai aln_illumina_2.sai $illumina_1 $illumina_2 > illumina_pacbio_alignment.sam
+
+# Convert the output sam file to bam file
+samtools view -b -o illumina_pacbio_alignment.bam illumina_pacbio_alignment.sam
