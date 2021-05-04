bf274807035a9dca33a30eae03ac74fbc2cd5aa6
Ida Wettergren <ida@rackham3.uppmax.uu.se>
2021-05-04 15:18:21 +0200

    updated trimmomatic script

--- code/trimmomatic.sh
+++ code/trimmomatic.sh
@@ -15,3 +15,14 @@ modules load trimmomatic
 # Paths to files
 reads_forward="/home/ida/genome_analysis/project/data/raw_data/transcriptome/untrimmed/SRR6040095_scaffold_10.1.fastq.gz"
 reads_reverse="/home/ida/genome_analysis/project/data/raw_data/transcriptome/untrimmed/SRR6040095_scaffold_10.2.fastq.gz"
+
+# Names for output files
+out_forward_unpaired="SRR6040095_scaffold_10.1U.fastq.gz"
+out_forward_paired="SRR6040095_scaffold_10.1P.fastq.gz"
+out_reverse_unpaired="SRR6040095_scaffold_10.2U.fastq.gz"
+out_reverse_paired="SRR6040095_scaffold_10.2P.fastq.gz"
+
+
+# Run Trimmomatic (trim Illumina sequences and leading/trailing sequences below 3 quality)
+java -jar $TRIMMOMATIC_HOME/trimmomatic.jar PE -threads 2 $reads_forward $reads_reverse $out_forward_paired $out_forward_unpaired \
+$out_reverse_paired $out_reverse_unpaired ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3
