9c592af1a627aba74e11917476b2048c83301077
Ida Wettergren <ida@rackham2.uppmax.uu.se>
2022-05-19 11:50:55 +0200

    updated script

--- code/trimmomatic.sh
+++ code/trimmomatic.sh
@@ -4,13 +4,13 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 01:00:00
-#SBATCH -J trimmomatic_rna
+#SBATCH -J trimmomatic
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user ida.wettergren.8542@student.uu.se
 
 # Load modules
-modules load bioinfo-tools
-modules load trimmomatic
+module load bioinfo-tools
+module load trimmomatic
 
 # Paths to files
 reads_forward="/home/ida/genome_analysis/project/data/raw_data/transcriptome/untrimmed/SRR6040095_scaffold_10.1.fastq.gz"
@@ -22,7 +22,6 @@ out_forward_paired="SRR6040095_scaffold_10.1P.fastq.gz"
 out_reverse_unpaired="SRR6040095_scaffold_10.2U.fastq.gz"
 out_reverse_paired="SRR6040095_scaffold_10.2P.fastq.gz"
 
-
 # Run Trimmomatic (trim Illumina sequences and leading/trailing sequences below 3 quality)
 java -jar $TRIMMOMATIC_HOME/trimmomatic.jar PE -threads 2 $reads_forward $reads_reverse $out_forward_paired $out_forward_unpaired \
-$out_reverse_paired $out_reverse_unpaired ILLUMINACLIP:TruSeq3-PE.fa:2:30:10:2:keepBothReads LEADING:3 TRAILING:3
+$out_reverse_paired $out_reverse_unpaired ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:10 TRAILING:10
